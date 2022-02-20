9458a14a4cc5ec48a099435f14c9f15c58330d13
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-02-20 16:25:59 +0100

    fix: applied scalafmt, added neo4j connection config

--- src/main/scala/com/freelanceStats/components/aliasResolver/neo4j/PositionTypeAliasResolver.scala
+++ src/main/scala/com/freelanceStats/components/aliasResolver/neo4j/PositionTypeAliasResolver.scala
@@ -3,13 +3,17 @@ package com.freelanceStats.components.aliasResolver.neo4j
 import com.freelanceStats.commons.models.indexedJob.PositionType
 import com.freelanceStats.configurations.Neo4jConfiguration
 import com.freelanceStats.models.SourceAlias
-import com.freelanceStats.util.neo4jConversions.{ReferencedByAliasConversion, SourceAliasQueryArgMapper, SourceAliasResultMapper}
+import com.freelanceStats.util.neo4jConversions.{
+  ReferencedByAliasConversion,
+  SourceAliasQueryArgMapper,
+  SourceAliasResultMapper
+}
 import neotypes.{QueryArgMapper, mappers}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
-class PositionTypeAliasResolver @Inject()(
+class PositionTypeAliasResolver @Inject() (
     override val configuration: Neo4jConfiguration
 )(
     override implicit val executionContext: ExecutionContext
--- src/main/scala/com/freelanceStats/components/aliasResolver/neo4j/TimezoneAliasResolver.scala
+++ src/main/scala/com/freelanceStats/components/aliasResolver/neo4j/TimezoneAliasResolver.scala
@@ -3,13 +3,17 @@ package com.freelanceStats.components.aliasResolver.neo4j
 import com.freelanceStats.commons.models.indexedJob.Timezone
 import com.freelanceStats.configurations.Neo4jConfiguration
 import com.freelanceStats.models.SourceAlias
-import com.freelanceStats.util.neo4jConversions.{ReferencedByAliasConversion, SourceAliasQueryArgMapper, SourceAliasResultMapper}
+import com.freelanceStats.util.neo4jConversions.{
+  ReferencedByAliasConversion,
+  SourceAliasQueryArgMapper,
+  SourceAliasResultMapper
+}
 import neotypes.{QueryArgMapper, mappers}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
-class TimezoneAliasResolver @Inject()(
+class TimezoneAliasResolver @Inject() (
     override val configuration: Neo4jConfiguration
 )(
     override implicit val executionContext: ExecutionContext
