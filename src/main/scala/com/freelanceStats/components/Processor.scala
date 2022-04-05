cf0312d36875cf849c0a447e94c750a429ca79ff
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2022-04-05 21:30:29 +0200

    Added job validation to processor, implemented better error logging

--- src/main/scala/com/freelanceStats/components/Processor.scala
+++ src/main/scala/com/freelanceStats/components/Processor.scala
@@ -1,7 +1,6 @@
 package com.freelanceStats.components
 
 import akka.Done
-import akka.stream.scaladsl.GraphDSL.Builder
 import akka.stream.scaladsl.{
   Broadcast,
   Flow,
@@ -16,6 +15,7 @@ import akka.stream.{ClosedShape, KillSwitch, KillSwitches, OverflowStrategy}
 import akka.util.ByteString
 import com.freelanceStats.commons.models.RawJob
 import com.freelanceStats.commons.models.indexedJob.IndexedJob
+import com.freelanceStats.components.JobValidator.ErrorMessage
 import com.freelanceStats.components.elasticClient.ElasticClient
 import com.freelanceStats.components.indexedJobCreator.IndexedJobCreator
 import com.freelanceStats.configurations.ApplicationConfiguration
@@ -30,6 +30,7 @@ class Processor @Inject() (
     s3Client: S3Client,
     elasticClient: ElasticClient,
     jobCreator: IndexedJobCreator,
+    jobValidator: JobValidator,
     applicationConfiguration: ApplicationConfiguration
 ) {
 
@@ -37,53 +38,46 @@ class Processor @Inject() (
     queueClient.source
       .viaMat(KillSwitches.single)(Keep.right)
 
-  private def sink: Sink[Either[IndexingError, IndexingSuccess], Future[Done]] =
-    Flow[Either[IndexingError, IndexingSuccess]]
-      .collect { case Right(IndexingSuccess(job)) =>
-        job
-      }
+  private def sink: Sink[IndexedJob, Future[Done]] =
+    Flow[IndexedJob]
       .via(elasticClient.bulkIndex)
       .toMat(Sink.ignore)(Keep.right)
 
-  private def s3DocumentFetch()(implicit builder: Builder[_]) =
-    builder.add(
-      Flow[RawJob]
-        .flatMapConcat { job =>
-          Source
-            .single(job.fileReference)
-            .via(s3Client.getFlow)
-            .map {
-              case (reference, Some(source)) => reference -> source
-              case (reference, None) =>
-                throw IndexingError(
-                  job,
-                  new Exception(s"File reference: '$reference' wasn't found")
-                )
-            }
-        }
-        .buffer(
-          applicationConfiguration.batchElementsMax,
-          OverflowStrategy.backpressure
-        )
-    )
+  private def s3DocumentFetch() =
+    Flow[RawJob]
+      .flatMapConcat { job =>
+        Source
+          .single(job.fileReference)
+          .via(s3Client.getFlow)
+          .map {
+            case (reference, Some(source)) => reference -> source
+            case (reference, None) =>
+              throw IndexingError(
+                job,
+                new Exception(s"File reference: '$reference' wasn't found")
+              )
+          }
+      }
+      .buffer(
+        applicationConfiguration.batchElementsMax,
+        OverflowStrategy.backpressure
+      )
 
-  private def elasticJobFetch()(implicit builder: Builder[_]) =
-    builder.add(
-      elasticClient.findDocumentsBySourceAndSourceId
-        .map {
-          case Left(error)     => throw error
-          case Right(maybeJob) => maybeJob
-        }
-    )
+  private def elasticJobFetch() =
+    elasticClient.findDocumentsBySourceAndSourceId
+      .map {
+        case Left(error)     => throw error
+        case Right(maybeJob) => maybeJob
+      }
 
   def apply(): RunnableGraph[(KillSwitch, Future[Done])] =
     RunnableGraph.fromGraph(
       GraphDSL.createGraph(source, sink)(Keep.both) {
-        implicit builder => (source, sink) =>
+        implicit builder => (sourceShape, sinkShape) =>
           import GraphDSL.Implicits._
 
-          val inputBroadcast = builder.add(Broadcast[RawJob](3))
-          val zip = builder.add(
+          val broadcastShape = builder.add(Broadcast[RawJob](3))
+          val zipShape = builder.add(
             ZipWith[
               RawJob,
               Option[IndexedJob],
@@ -96,39 +90,57 @@ class Processor @Inject() (
             ](Tuple3.apply)
           )
 
-          val rawJobBuffer = builder.add(
+          val flowShape = builder.add(
             Flow[RawJob]
               .buffer(
                 applicationConfiguration.batchElementsMax,
                 OverflowStrategy.backpressure
               )
           )
-          val indexingErrorRecovery = builder.add(
+          val flowShape1 = builder.add(s3DocumentFetch())
+          val flowShape2 = builder.add(elasticJobFetch())
+          val flowShape3 = builder.add(jobCreator())
+          val flowShape4 = builder.add(
             Flow[Either[IndexingError, IndexingSuccess]]
-              .recover { case error: IndexingError =>
-                Left(error)
+              .divertTo(
+                Flow[Either[IndexingError, IndexingSuccess]]
+                  .collect { case Left(error) =>
+                    error
+                  }
+                  .log("job-creator-error-logger")
+                  .to(Sink.ignore),
+                _.isLeft
+              )
+              .collect { case Right(IndexingSuccess(job)) =>
+                job
+              }
+          )
+          val flowShape5 = builder.add(jobValidator())
+          val flowShape6 = builder.add(
+            Flow[Either[ErrorMessage, IndexedJob]]
+              .divertTo(
+                Flow[Either[ErrorMessage, IndexedJob]]
+                  .collect { case Left(error) =>
+                    error
+                  }
+                  .log("job-validator-error-logger")
+                  .to(Sink.ignore),
+                _.isLeft
+              )
+              .collect { case Right(job) =>
+                job
               }
           )
 
-          val _s3DocumentFetch = s3DocumentFetch()
-          val _elasticJobFetch = elasticJobFetch()
-          val _jobCreator = builder.add(jobCreator())
+          //  @formatter:off
 
-          source.out ~> inputBroadcast.in
+          sourceShape ~> broadcastShape.in  ; broadcastShape.out(0) ~>  flowShape   ~>  zipShape.in0
+                                              broadcastShape.out(1) ~>  flowShape1  ~>  zipShape.in2
+                                              broadcastShape.out(2) ~>  flowShape2  ~>  zipShape.in1  ; zipShape.out  ~>  flowShape3
 
-          inputBroadcast.out(0) ~> rawJobBuffer.in
-          inputBroadcast.out(1) ~> _elasticJobFetch.in
-          inputBroadcast.out(2) ~> _s3DocumentFetch.in
+          flowShape3  ~>  flowShape4  ~>  flowShape5  ~>  flowShape6  ~> sinkShape
 
-          rawJobBuffer.out ~> zip.in0
-          _elasticJobFetch ~> zip.in1
-          _s3DocumentFetch ~> zip.in2
-
-          zip.out ~> _jobCreator.in
-
-          _jobCreator.out ~> indexingErrorRecovery.in
-
-          indexingErrorRecovery.out ~> sink.in
+          //  @formatter:on
 
           ClosedShape
       }
